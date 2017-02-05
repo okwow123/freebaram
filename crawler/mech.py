96d626ece7f89bcf724acdf16631c5002881dd6c
okwow123 <okwow123@naver.com>
2017-02-05 14:56:20 +0900

    change max_no algorithm

--- crawler/mech.py
+++ crawler/mech.py
@@ -1,28 +1,28 @@
+'''
+how to find top 30 address from freebaram site?
+'''
+
 import mechanize
 from bs4 import BeautifulSoup
 import urllib2 
 import cookielib
-# div id ="view_content" 
-
+#1.go to site with login
 cj = cookielib.CookieJar()
 br = mechanize.Browser()
 br.set_cookiejar(cj)
 br.open('http://broadcamp.com/bbs/login.php')
-#br.open('http://broadcamp.com/bbs/board.php?bo_table=d4')
 br.select_form(nr=0)
 br.form['mb_id'] = 'okwow123'
 br.form['mb_password'] = '239184'
 br.submit()
-#br.open('http://broadcamp.com/bbs/board.php?bo_table=d4&wr_id=3751')
 br.open('http://broadcamp.com/bbs/board.php?bo_table=d4')
+#2.save scratch results
 result=br.response().read()
+#3.find max no 
 first_index= result.find('http://broadcamp.com/bbs/board.php?bo_table=d4&wr_id=')
-#no max value?
+max_no=result[first_index+53:first_index+57]
+print max_no
 print result[first_index:first_index+57]
-#session_requests = requests.session()
-#br.open("http://broadcamp.com/bbs/board.php?bo_table=d4")
-#print br.response().read()
-#result=session_requests.get('http://broadcamp.com/bbs/board.php?bo_table=d4')
-#print result.text
-#print br.response().read()
+#4.find 30 url address
+
 
