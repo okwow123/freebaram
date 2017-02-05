3f2ef7da3d469a847c04134a1151bc7ed3c9d5e7
okwow123 <okwow123@naver.com>
2017-02-05 16:39:28 +0900

    find 30 url

--- crawler/mech.py
+++ crawler/mech.py
@@ -6,6 +6,7 @@ import mechanize
 from bs4 import BeautifulSoup
 import urllib2 
 import cookielib
+import re
 #1.go to site with login
 cj = cookielib.CookieJar()
 br = mechanize.Browser()
@@ -21,8 +22,14 @@ result=br.response().read()
 #3.find max no 
 first_index= result.find('http://broadcamp.com/bbs/board.php?bo_table=d4&wr_id=')
 max_no=result[first_index+53:first_index+57]
-print max_no
-print result[first_index:first_index+57]
+#print result[first_index:first_index+57]
 #4.find 30 url address
-
+print "30 url?"
+for x in range(1):
+    taddress=result[first_index:first_index+53]+str(int(max_no)-x)
+    br.open(taddress)
+    tresult=br.response().read()
+    print tresult.split('<div id=\"view_content\">')[1].split('<!--view_content-->[0]')
+#tresult=re.search('<div id=\"view_content\">[1].split<!--view_content-->[0]',tresult)
+#print tresult.group()
 
