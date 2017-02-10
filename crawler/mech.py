33f248a604210401d0c826f8412c9fb2c056d592
okwow123 <okwow123@naver.com>
2017-02-10 22:42:38 +0900

    mech.py

--- crawler/mech.py
+++ crawler/mech.py
@@ -24,12 +24,16 @@ first_index= result.find('http://broadcamp.com/bbs/board.php?bo_table=d4&wr_id='
 max_no=result[first_index+53:first_index+57]
 #print result[first_index:first_index+57]
 #4.find 30 url address
-print "30 url?"
-for x in range(1):
+for x in range(30):
     taddress=result[first_index:first_index+53]+str(int(max_no)-x)
     br.open(taddress)
     tresult=br.response().read()
-    print tresult.split('<div id=\"view_content\">')[1].split('<!--view_content-->[0]')
+    ttresult=tresult.split('<div id=\"view_content\">')[1].split('<!--view_content-->[0]')
+    urls=re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tresult)
+    for y in range(len(urls)):
+        if urls[y].find("broadcamp") == -1 and urls[y].find("google") == -1 and urls[y].find("schema") == -1 and  urls[y].find("miwit") == -1 and  urls[y].find("-") == -1 and  urls[y].find("<") == -1 and  urls[y].find("image") == -1:
+            print urls[y]
+    print "******************************************"        
 #tresult=re.search('<div id=\"view_content\">[1].split<!--view_content-->[0]',tresult)
 #print tresult.group()
 
