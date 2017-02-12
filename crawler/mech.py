1852863130d179d24c30e36fe1575c40d215a130
okwow123 <okwow123@naver.com>
2017-02-12 21:13:52 +0900

    30 extracts success

--- crawler/mech.py
+++ crawler/mech.py
@@ -1,12 +1,14 @@
-'''
-how to find top 30 address from freebaram site?
-'''
-
+# -*- coding: utf8 -*-
 import mechanize
 from bs4 import BeautifulSoup
 import urllib2 
 import cookielib
 import re
+import sqlite3
+conn=sqlite3.connect('/home/top10.db')
+conn.text_factory = lambda x: unicode(x, 'utf-8', 'ignore')
+item = [[u'']*3 for x in xrange(30)]
+index=0
 #1.go to site with login
 cj = cookielib.CookieJar()
 br = mechanize.Browser()
@@ -23,17 +25,38 @@ result=br.response().read()
 first_index= result.find('http://broadcamp.com/bbs/board.php?bo_table=d4&wr_id=')
 max_no=result[first_index+53:first_index+57]
 #print result[first_index:first_index+57]
-#4.find 30 url address
+#4.find 30 address,title,opendate
 for x in range(30):
     taddress=result[first_index:first_index+53]+str(int(max_no)-x)
     br.open(taddress)
     tresult=br.response().read()
+    title=tresult.split('<title>')[1].split('</title>')
+    temp=title[0].replace('> 사이트 추천등록 | 브로드캠프','') 
+    item[index][1]=temp
     ttresult=tresult.split('<div id=\"view_content\">')[1].split('<!--view_content-->[0]')
-    urls=re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', tresult)
+    match=re.search(r'(\d+월 +\d+일)',tresult)
+    if match:
+        mnd = match.group(1).replace('월','/').replace('일','')
+        item[index][2]=mnd
+    match=re.search(r'(\d+/+\d)',tresult)
+    if match:
+        mnd = match.group(1)
+        item[index][2]=mnd
+    item[index][2]=mnd.replace(' ','')
+    urls=re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', ttresult[0])
     for y in range(len(urls)):
-        if urls[y].find("broadcamp") == -1 and urls[y].find("google") == -1 and urls[y].find("schema") == -1 and  urls[y].find("miwit") == -1 and  urls[y].find("-") == -1 and  urls[y].find("<") == -1 and  urls[y].find("image") == -1:
-            print urls[y]
-    print "******************************************"        
+        if urls[y].find("broadcamp") == -1 and urls[y].find("google") == -1 and urls[y].find("schema") == -1 and  urls[y].find("miwit") == -1 and  urls[y].find("-") == -1 and  urls[y].find("<") == -1 and  urls[y].find("image") == -1 and urls[y] != '' and len(urls[y])>5:
+            item[index][0]=urls[y]
+            #br.open(urls[y])
+            #tttresult=br.response().read()
+            break
+    conn.execute('insert into top10(address,name,date) values (?,?,?)',item[index])
+    index+=1
+conn.commit()
+'''
+for x in range(30):
+    print item[x][0]+' '+item[x][1]+' '+item[x][2]
+'''
 #tresult=re.search('<div id=\"view_content\">[1].split<!--view_content-->[0]',tresult)
 #print tresult.group()
 
--- crawler/str.py
+++ crawler/str.py
@@ -1,6 +1,8 @@
-result='free baram open 2017 year 02/17 go server'
-rresult=result.split(' ')
-print rresult
-for x in range(len(rresult)):
-    if rresult[x].find("server") != -1:
-        print rresult[x]
+# -*- coding: utf8 -*-
+import re
+result='free baram open 2017 year 2월 17 go server'
+#rresult=result.split(' ')
+#print rresult
+match=re.search(r'(\d+월 +\d+일)',result)
+if match:
+    print match.groups()
